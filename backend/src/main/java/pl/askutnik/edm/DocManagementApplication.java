31e78cad14978496d9c18f5b5ec103fca69f761c
askutnik <albertskutnik@gmail.com>
2026-07-23 22:10:52 +0200

    docs: backend folders tree

--- backend/src/main/java/pl/askutnik/edm/DocManagementApplication.java
+++ backend/src/main/java/pl/askutnik/edm/DocManagementApplication.java
@@ -1,12 +1,29 @@
 package pl.askutnik.edm;
 
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DocManagementApplication {
 
+    private final Path uploadDirectory;
+
+    public DocManagementApplication(@Value("{app.upload-dir}") String uploadDirectory) throws Exception {
+        this.uploadDirectory = Path.of(uploadDirectory).toAbsolutePath().normalize();
+        Files.createDirectories(this.uploadDirectory);
+    }
+    
+
+
     public static void main(String[] args) {
         SpringApplication.run(DocManagementApplication.class, args);
     }
+
+
+    
+
 }
